e2592dd3b605a3174aac00950870525b6bc6c73f
Christian Artin <cartin@genetec.com>
2025-05-30 15:35:31 -0400

    feat: add support for app namespace
    Signed-off-by: Christian Artin <cartin@genetec.com>

--- pkg/controller/applications/controller.go
+++ pkg/controller/applications/controller.go
@@ -122,6 +122,13 @@ func (e *external) Observe(ctx context.Context, mg resource.Managed) (managed.Ex
 
 	appQuery := application.ApplicationQuery{
 		Name: &name,
+		Projects: []string{
+			cr.Spec.ForProvider.Project,
+		},
+	}
+
+	if cr.Spec.ForProvider.AppNamespace != "" {
+		appQuery.AppNamespace = &cr.Spec.ForProvider.AppNamespace
 	}
 
 	// we have to use List() because Get() returns permission error
@@ -130,15 +137,16 @@ func (e *external) Observe(ctx context.Context, mg resource.Managed) (managed.Ex
 	if err != nil {
 		return managed.ExternalObservation{}, errors.Wrap(err, errListFailed)
 	}
-	app := &argocdv1alpha1.Application{}
-	for _, item := range apps.Items {
-		if item.Name == name && item.Spec.Project == cr.Spec.ForProvider.Project {
-			app = item.DeepCopy()
-		}
+
+	if len(apps.Items) == 0 {
+		return managed.ExternalObservation{
+			ResourceExists:   false,
+			ResourceUpToDate: false,
+		}, nil
+	} else if len(apps.Items) > 1 {
+		return managed.ExternalObservation{}, errors.New("multiple applications found")
 	}
-	if app.Name == "" {
-		return managed.ExternalObservation{}, nil
-	}
+	app := &apps.Items[0]
 
 	current := cr.Spec.ForProvider.DeepCopy()
 	lateInitialize(&cr.Spec.ForProvider, app)
@@ -189,7 +197,12 @@ func (e *external) Delete(ctx context.Context, mg resource.Managed) (managed.Ext
 		return managed.ExternalDelete{}, errors.New(errNotApplication)
 	}
 	query := application.ApplicationDeleteRequest{
-		Name: clients.StringToPtr(meta.GetExternalName(cr)),
+		Name:    clients.StringToPtr(meta.GetExternalName(cr)),
+		Project: &cr.Spec.ForProvider.Project,
+	}
+
+	if cr.Spec.ForProvider.AppNamespace != "" {
+		query.AppNamespace = &cr.Spec.ForProvider.AppNamespace
 	}
 
 	_, err := e.client.Delete(ctx, &query)
@@ -232,6 +245,10 @@ func generateCreateApplicationRequest(cr *v1alpha1.Application) *application.App
 		Spec: *spec,
 	}
 
+	if cr.Spec.ForProvider.AppNamespace != "" {
+		app.SetNamespace(cr.Spec.ForProvider.AppNamespace)
+	}
+
 	repoCreateRequest := &application.ApplicationCreateRequest{
 		Application: app,
 	}
@@ -252,6 +269,10 @@ func generateUpdateRepositoryOptions(cr *v1alpha1.Application) *application.Appl
 			Finalizers:  cr.Spec.ForProvider.Finalizers,
 		},
 		Spec: *spec,
+	}
+
+	if cr.Spec.ForProvider.AppNamespace != "" {
+		app.SetNamespace(cr.Spec.ForProvider.AppNamespace)
 	}
 
 	o := &application.ApplicationUpdateRequest{
