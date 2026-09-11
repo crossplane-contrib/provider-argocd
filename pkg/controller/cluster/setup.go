5e2b585b80f413c1462aa48c4ce14c1ea4871cba
Christian Artin <gravufo@gmail.com>
2026-09-10 23:27:18 -0400

    feat: implement provider safe start
    Signed-off-by: Christian Artin <gravufo@gmail.com>

--- pkg/controller/cluster/setup.go
+++ pkg/controller/cluster/setup.go
@@ -18,8 +18,14 @@ package cluster
 
 import (
 	xpcontroller "github.com/crossplane/crossplane-runtime/v2/pkg/controller"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
 
+	applicationsv1alpha1 "github.com/crossplane-contrib/provider-argocd/apis/cluster/applications/v1alpha1"
+	applicationsetsv1alpha1 "github.com/crossplane-contrib/provider-argocd/apis/cluster/applicationsets/v1alpha1"
+	clusterv1alpha1 "github.com/crossplane-contrib/provider-argocd/apis/cluster/cluster/v1alpha1"
+	projectsv1alpha1 "github.com/crossplane-contrib/provider-argocd/apis/cluster/projects/v1alpha1"
+	repositoriesv1alpha1 "github.com/crossplane-contrib/provider-argocd/apis/cluster/repositories/v1alpha1"
 	"github.com/crossplane-contrib/provider-argocd/pkg/controller/cluster/applications"
 	"github.com/crossplane-contrib/provider-argocd/pkg/controller/cluster/applicationsets"
 	"github.com/crossplane-contrib/provider-argocd/pkg/controller/cluster/cluster"
@@ -34,16 +40,40 @@ import (
 func Setup(mgr ctrl.Manager, o xpcontroller.Options) error {
 	for _, setup := range []func(ctrl.Manager, xpcontroller.Options) error{
 		config.Setup,
-		repositories.Setup,
-		projects.Setup,
-		cluster.Setup,
-		applications.Setup,
-		applicationsets.Setup,
-		tokens.Setup,
+		func(m ctrl.Manager, o xpcontroller.Options) error {
+			return setupGated(m, o, repositories.Setup, repositoriesv1alpha1.RepositoryGroupVersionKind)
+		},
+		func(m ctrl.Manager, o xpcontroller.Options) error {
+			return setupGated(m, o, projects.Setup, projectsv1alpha1.ProjectGroupVersionKind)
+		},
+		func(m ctrl.Manager, o xpcontroller.Options) error {
+			return setupGated(m, o, cluster.Setup, clusterv1alpha1.ClusterGroupVersionKind)
+		},
+		func(m ctrl.Manager, o xpcontroller.Options) error {
+			return setupGated(m, o, applications.Setup, applicationsv1alpha1.ApplicationGroupVersionKind)
+		},
+		func(m ctrl.Manager, o xpcontroller.Options) error {
+			return setupGated(m, o, applicationsets.Setup, applicationsetsv1alpha1.ApplicationSetGroupVersionKind)
+		},
+		func(m ctrl.Manager, o xpcontroller.Options) error {
+			return setupGated(m, o, tokens.Setup, projectsv1alpha1.TokenGroupVersionKind)
+		},
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
 		}
 	}
+	return nil
+}
+
+func setupGated(mgr ctrl.Manager, o xpcontroller.Options, setup func(ctrl.Manager, xpcontroller.Options) error, gvk schema.GroupVersionKind) error {
+	if o.Gate == nil {
+		return setup(mgr, o)
+	}
+	o.Gate.Register(func() {
+		if err := setup(mgr, o); err != nil {
+			panic(err)
+		}
+	}, gvk)
 	return nil
 }
